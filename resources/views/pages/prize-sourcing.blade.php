6ceb3a82ac31712f8a33d89f4de3b63bf053d227
charles <charles@promotionsinteractive.com>
2019-08-28 17:56:00 +0100

    start prize sourcing styling

--- resources/views/pages/prize-sourcing.blade.php
+++ resources/views/pages/prize-sourcing.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.app')
 @section('content')
 
-<div class="in-content-hero" style="background:url(img/prize-sourcing-for-promotional-campaigns.jpg) no-repeat; background-size: cover;"></div>
+<div class="in-content-hero prize-sourcing-top-hero" style=""></div>
 
 <div class="container">
         <div class="heading-text">
@@ -12,37 +12,41 @@
                 </p> 
         </div>
         <div class="row">
-                <p>
-                    Merchandise, products, vouchers, holidays, experiences. A prize promotion, competition 
-                    or even an internal rewards scheme needs the right prize for the campaign. 
-                    After all, it’s the prize that gets customers excited and more likely to buy your product 
-                    or enter your competition. We know choosing the right prize is exceptionally important 
-                    since it must appeal to your audience while also staying on budget, which can be a challenge.
-                </p>
-                <p>
-                    Here at Prize Professionals, we love a challenge! We’ve sourced a wide array of prizes
-                    for an impressive range of brands, from large quantities of merchandise to organising 
-                    life-changing trips around the world. If you have something in mind, great! 
-                    Let us know what you want and we’ll source it for you at the very best price. 
-                    Know your audience but not sure what will appeal to them? No problem, 
-                    our teams have extensive experience and we’re not afraid of a little research. 
-                    Perhaps you have no ideas for a prize at all and rather let us handle the whole thing;
-                    this tends to be the most popular option with clients and partner agencies. 
-                    With a wealth of experience and a large range of contacts, we can put something together 
-                    that is totally bespoke and is guaranteed to get your customers excited. 
-                </p>
-                <p>
-                    Once we have agreed a prize, next comes the fulfilment; which we handle completely. 
-                    We can contact winners on your behalf to let them know the good news, to arrange delivery, 
-                    and to make sure they are completely happy from start to finish. From the logistics of getting
-                    the prize to your winners to making sure everything runs smoothly, leave it all to us.
-                </p>
-                <p>
-                    It doesn’t matter where in the world your winners are, we will ensure their prize gets to them.
-                    We’re happy to arrange global delivery, meaning you’re not limited to certain localities and 
-                    can open up your promotion to a much wider, global audience. Let us worry about how to get your 
-                    prizes to your winners in the far corners of the Earth, it’s what we do best.
-                </p>
+            <div class="col-md-12">
+                    <p>
+                        Merchandise, products, vouchers, holidays, experiences. A prize promotion, competition 
+                        or even an internal rewards scheme needs the right prize for the campaign. 
+                        After all, it’s the prize that gets customers excited and more likely to buy your product 
+                        or enter your competition. We know choosing the right prize is exceptionally important 
+                        since it must appeal to your audience while also staying on budget, which can be a challenge.
+                    </p>
+                    <p>
+                        Here at Prize Professionals, we love a challenge! We’ve sourced a wide array of prizes
+                        for an impressive range of brands, from large quantities of merchandise to organising 
+                        life-changing trips around the world. If you have something in mind, great! 
+                        Let us know what you want and we’ll source it for you at the very best price. 
+                        Know your audience but not sure what will appeal to them? No problem, 
+                        our teams have extensive experience and we’re not afraid of a little research. 
+                        Perhaps you have no ideas for a prize at all and rather let us handle the whole thing;
+                        this tends to be the most popular option with clients and partner agencies. 
+                        With a wealth of experience and a large range of contacts, we can put something together 
+                        that is totally bespoke and is guaranteed to get your customers excited. 
+                    </p>
+                    <p>
+                        Once we have agreed a prize, next comes the fulfilment; which we handle completely. 
+                        We can contact winners on your behalf to let them know the good news, to arrange delivery, 
+                        and to make sure they are completely happy from start to finish. From the logistics of getting
+                        the prize to your winners to making sure everything runs smoothly, leave it all to us.
+                    </p>
+                    <p>
+                        It doesn’t matter where in the world your winners are, we will ensure their prize gets to them.
+                        We’re happy to arrange global delivery, meaning you’re not limited to certain localities and 
+                        can open up your promotion to a much wider, global audience. Let us worry about how to get your 
+                        prizes to your winners in the far corners of the Earth, it’s what we do best.
+                    </p>
+            </div>
+              
+               
         </div>
 
         <div class="heading-text">
@@ -53,8 +57,8 @@
                         <img src="{{asset('img/tangible-products.svg')}}" class="card-img-top" alt="">
                         <div class="card-body">
                             <h5 class="card-title">Tangible products</h5>
-                            <p class="card-text">If it’s tangible, we can source it for you, from electronics to clothing and even cars, let us sort it for you </p>
-                            <a href="#" class="btn button">FIND OUT MORE</a>
+                            <p class="card-text tangible-products-text">If it’s tangible, we can source it for you, from electronics to clothing and even cars, let us sort it for you </p>
+                            <a href="#" class="btn btn-pp">FIND OUT MORE</a>
                         </div>
                 </div>
                 <div class="card col-md-4" style="">
@@ -62,7 +66,7 @@
                         <div class="card-body">
                             <h5 class="card-title">Exhilarating experiences</h5>
                             <p class="card-text">Worldwide experiences that will create lasting memories, we can source just about anything you can think of </p>
-                            <a href="#" class="btn button">FIND OUT MORE</a>
+                            <a href="#" class="btn btn-pp">FIND OUT MORE</a>
                         </div>
                 </div>
                 <div class="card col-md-4" style="">
@@ -70,7 +74,7 @@
                         <div class="card-body">
                             <h5 class="card-title">Once in a life time trips</h5>
                             <p class="card-text">Worldwide experiences that will create lasting memories, we can source just about anything you can think of </p>
-                            <a href="#" class="btn button">FIND OUT MORE</a>
+                            <a href="#" class="btn btn-pp">FIND OUT MORE</a>
                         </div>
                 </div>
         </div>
@@ -81,7 +85,7 @@
                         <div class="card-body">
                             <h5 class="card-title">Holidays & getaways</h5>
                             <p class="card-text">Let us create a dream holiday your customers are going to want to win, increasing chances they’ll enter </p>
-                            <a href="#" class="btn button">FIND OUT MORE</a>
+                            <a href="#" class="btn btn-pp">FIND OUT MORE</a>
                         </div>
                 </div>
                 <div class="card col-md-4" style="">
@@ -89,7 +93,7 @@
                         <div class="card-body">
                             <h5 class="card-title">Vouchers for anywhere</h5>
                             <p class="card-text">Give the gift of prize freedom with vouchers for almost anywhere, presented either digitally or printed</p>
-                            <a href="#" class="btn button">FIND OUT MORE</a>
+                            <a href="#" class="btn btn-pp">FIND OUT MORE</a>
                         </div>
                 </div>
                 <div class="card col-md-4" style="">
@@ -97,32 +101,35 @@
                         <div class="card-body">
                             <h5 class="card-title">Merchandise</h5>
                             <p class="card-text">We can create your very own branded merchandise on almost anything to reflect your own brand </p>
-                            <a href="#" class="btn button">FIND OUT MORE</a>
+                            <a href="#" class="btn btn-pp">FIND OUT MORE</a>
                         </div>
                 </div>
         </div>
 
 </div>
 
-<div class="in-content-hero" style="background:url(img/what-we-can-do-for-you-prize-sourcing.jpg) no-repeat; background-size: cover;"></div>
+<div class="in-content-hero prize-sourcing-what-we-can-do-hero" style=""></div>
 
 <div class="container">
         <div class="heading-text">
                 <h2>What we can do for you </h2>
         </div>
         <div class="row">
-            <p>
-                We will source the prize that will appeal to your target audience the most. 
-                The prize that will create excitement about the campaign, encourage entry 
-                but also build long lasting bonds between your brand and customers. 
-                Finding the right prize isn’t just about finding something flashy or high value, 
-                it’s about sourcing the prize that will excite the audience and increase chances of entry.   
-            </p>
-            <p>
-                Prize sourcing is limitless and we can provide everything from high-value items like cars and travel,
-                holidays, getaways and experience days, as well as much lower-cost items such as consumer goods, 
-                technology, merchandise; anything you can think of and everything you can’t.
-            </p>
+            <div class="col-md-12">
+                    <p>
+                        We will source the prize that will appeal to your target audience the most. 
+                        The prize that will create excitement about the campaign, encourage entry 
+                        but also build long lasting bonds between your brand and customers. 
+                        Finding the right prize isn’t just about finding something flashy or high value, 
+                        it’s about sourcing the prize that will excite the audience and increase chances of entry.   
+                    </p>
+                    <p>
+                        Prize sourcing is limitless and we can provide everything from high-value items like cars and travel,
+                        holidays, getaways and experience days, as well as much lower-cost items such as consumer goods, 
+                        technology, merchandise; anything you can think of and everything you can’t.
+                    </p>
+            </div>
+            
             <div class="py-md-5">
                     <ul>
                         <li> Find the perfect prize for your campaign </li>
@@ -158,7 +165,7 @@
                 to adapt to work in a way that suits you. So get in touch today, tell us what you need,
                 and we’ll tell you how we can help. 
             </p>
-            <div class="py-md-5"><a href="#" class="btn button">Get in touch</a></div>
+            <div class="py-md-5"><a href="#" class="btn btn-pp">Get in touch</a></div>
         </div>
 </div>
 
@@ -223,7 +230,7 @@
                         <li> Specialist winner handling </li>
                         <li> End-to-end management</li>
                     </ul>
-                    <a href="#" class="btn button">Get in touch</a>
+                    <a href="#" class="btn btn-pp">Get in touch</a>
             </div>
           
         </div>
@@ -248,7 +255,7 @@
                     We’re flexible, though. So why not give us a call for a chat and we’ll work together to make sure your campaign does exactly what you want it to.
                 </p>
                 <div class="py-md-5">
-                        <a href="#" class="btn button">Request a quote</a>
+                        <a href="#" class="btn btn-pp">Request a quote</a>
                 </div>
         </div>
       
